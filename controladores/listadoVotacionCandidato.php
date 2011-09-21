1688444d4f39f5d5ba85d9fc191c1e62db938585
Luis A. Sanchez <armandon20@gmail.com>
2011-09-21 11:53:42 -0500

    Nueva implementacion para el punto 6. Faltan los reportes.

--- controladores/listadoVotacionCandidato.php
+++ controladores/listadoVotacionCandidato.php
@@ -1,5 +1,27 @@
-<?php 
+<?php
+	session_start();
 	header("Content-Type: text/plain");
-	$datos = array();
-	echo json_encode($datos);
+	require_once 'Configuracion.php';
+	require_once 'FunDivipol.php';
+	
+	$datos = json_decode(str_replace("\\","",$_POST['datos']));
+	$_SESSION['listadoVotacionCandidato'] = serialize($datos);
+	
+	require_once 'listadoVotacionCandidato_inc.php';
+	
+	
+	$arrCandVota = array();
+	while($row = ibase_fetch_object($result)){
+		$votaCand = array();
+		$votaCand['codigo']    = $row->CODIGO;
+		$votaCand['nombres']   = htmlentities($row->NOMBRES);
+		$votaCand['apellidos'] = htmlentities($row->APELLIDOS);
+		$votaCand['partido']   = htmlentities($row->DESCRIPCION);
+		$votaCand['votos']     = $row->VOTOS;
+		array_push($arrCandVota,$votaCand);
+	}
+	
+	ibase_free_result($result);
+	ibase_close($firebird);
+	echo json_encode($arrCandVota);
 ?>
