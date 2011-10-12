f5a8b199268c05546631d4462892dd08e2565344
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-10-12 09:36:31 -0500

    Punto 1, terminado con todos los informes.

--- controladores/informes/consolidadoPartidoLista_doc.php
+++ controladores/informes/consolidadoPartidoLista_doc.php
@@ -9,9 +9,71 @@
 	
 	require_once('consolidadoPartidoLista_inc.php');
 	
+	$partidos = array();
+	$candidatos = array();
 	
-	// //Creando el escritor
-	// $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'HTML');
-	// $objWriter->setSheetIndex(0);
-	// $objWriter->save('php://output');
+	while($row = ibase_fetch_object($result)) {
+		array_push($partidos,$row);
+	}
+	if($result1 != null){
+		while($row = ibase_fetch_object($result1)) {
+			array_push($candidatos,$row);
+		}
+	}
+	
+	//Configurancion de las cabeceras
+	$objPHPExcel = new PHPExcel();
+	
+	$objPHPExcel->getProperties()->setCreator("Ing. Luis A. Sanchez")
+							 ->setLastModifiedBy("Ing. Luis A. Sanchez")
+							 ->setTitle("Office 2007 XLSX Test Document")
+							 ->setSubject("Office 2007 XLSX Test Document")
+							 ->setDescription("Consolidado Partido Lista")
+							 ->setKeywords("office 2007 openxml")
+							 ->setCategory("");
+	
+	$objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'CODIGO')
+            ->setCellValue('B1', 'NOMBRE')
+            ->setCellValue('C1', 'VOTOS');
+	//Fin de las cabeceras
+	
+	
+	//Configurado del contenido
+	$cont = 2;
+	foreach($partidos as $partido) {
+		
+		$objPHPExcel->getActiveSheet()->setCellValue('A'.$cont,$partido->CODIGO);
+		$objPHPExcel->getActiveSheet()->setCellValue('B'.$cont,utf8_encode($partido->DESCRIPCION));
+		$objPHPExcel->getActiveSheet()->setCellValue('C'.$cont,$partido->VOTOS);
+		//Hacer un cambio de color para las celdas de los partidos
+		$cont++;
+		
+		foreach($candidatos as $candidato) {
+			if($candidato->CODPARTIDO == $partido->CODIGO) {
+				$objPHPExcel->getActiveSheet()->setCellValue('A'.$cont,$partido->CODIGO.'-'.$candidato->CODCANDIDATO);
+				$objPHPExcel->getActiveSheet()->setCellValue('B'.$cont,utf8_encode($candidato->DESCRIPCION));
+				$objPHPExcel->getActiveSheet()->setCellValue('C'.$cont,$candidato->VOTOS);
+				$cont++;
+			}
+		}
+	}
+	
+	$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+	//Fin del contenido
+	
+	
+	//Cerrando la conexion
+	ibase_free_result($result);
+	if($result1 != null){ibase_free_result($result1);}
+	ibase_close($firebird);
+	
+	
+	//Creando el escritor
+	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'HTML');
+	$objWriter->setSheetIndex(0);
+	$objWriter->save('php://output');
+	
 ?>
--- controladores/informes/consolidadoPartidoLista_pdf.php
+++ controladores/informes/consolidadoPartidoLista_pdf.php
@@ -1,92 +1,112 @@
 <?php
 	session_start();
 	require_once 'ConfiguracionTCPDF.php';
+	
 	$datos = unserialize($_SESSION['consolidadoPartidoLista']);
-	require_once('../consolidadoPartidoLista_inc.php');
+	require_once('consolidadoPartidoLista_inc.php');
+	
+	$partidos = array();
+	$candidatos = array();
+	
+	while($row = ibase_fetch_object($result)) {
+		array_push($partidos,$row);
+	}
+	if($result1 != null){
+		while($row = ibase_fetch_object($result1)) {
+			array_push($candidatos,$row);
+		}
+	}
 	
 	//////////////////////////////////////////TCPDF////////////////////////////////////////////////////////
-	//creo el nuevo documento pdf
-	// $pdf = new TCPDF($page_orientacion, $pdf_unit, $pdf_page_format, true, 'UTF-8', false);
+	$pdf = new TCPDF($page_orientacion, $pdf_unit, $pdf_page_format, true, 'UTF-8', false);
 	
-	// //la informacion del documento
-	// $pdf->SetCreator('TCPDF');
-	// $pdf->SetAuthor('Luis A. Nunez');
-	// $pdf->SetTitle('Estadisticas Electorales');
-	// $pdf->SetSubject('Consolidado Partido Listas');
-	// $pdf->SetKeywords('Votacion, Partido, Consolidado, Elecciones, Colombia');
+	//la informacion del documento
+	$pdf->SetCreator('TCPDF');
+	$pdf->SetAuthor('Luis A. Nunez');
+	$pdf->SetTitle('Estadisticas Electorales');
+	$pdf->SetSubject('Consolidado Partido Listas');
+	$pdf->SetKeywords('Votacion, Partido, Consolidado, Elecciones, Colombia');
 	
-	// $headerstring = utf8_encode("Consolidado Partido y Listas");
-	// $pdf->SetHeaderData($pathLogo, $logowidth, $headertitle, $headerstring);
+	$headerstring = utf8_encode("Consolidado Partido y Listas");
+	$pdf->SetHeaderData($pathLogo, $logowidth, $headertitle, $headerstring);
 	
-	// //La fuente para la cabecera y pie de pagina
-	// $pdf->setHeaderFont(Array($fontmain, '', $fontmainsize));
-	// $pdf->setFooterFont(Array($fontdata, '', $fontdatasize));
+	//La fuente para la cabecera y pie de pagina
+	$pdf->setHeaderFont(Array($fontmain, '', $fontmainsize));
+	$pdf->setFooterFont(Array($fontdata, '', $fontdatasize));
 	
-	// // Fuente por defecto
-	// $pdf->SetDefaultMonospacedFont($fontmnspace);
+	// Fuente por defecto
+	$pdf->SetDefaultMonospacedFont($fontmnspace);
 	
-	// //Los margenes de las pagina, cabecera y pie de pagina
-	// $pdf->SetMargins($marginleft, $margintop, $marginright);
-	// $pdf->SetHeaderMargin($marginheader);
-	// $pdf->SetFooterMargin($marginfooter);
+	//Los margenes de las pagina, cabecera y pie de pagina
+	$pdf->SetMargins($marginleft, $margintop, $marginright);
+	$pdf->SetHeaderMargin($marginheader);
+	$pdf->SetFooterMargin($marginfooter);
 
-	// //set auto page breaks
-	// $pdf->SetAutoPageBreak(TRUE, $marginbottom);
+	//set auto page breaks
+	$pdf->SetAutoPageBreak(TRUE, $marginbottom);
 
-	// //set image scale factor
-	// $pdf->setImageScale($imgscalert);
+	//set image scale factor
+	$pdf->setImageScale($imgscalert);
 
-	// //set some language-dependent strings
-	// $pdf->setLanguageArray($l);
-	// //////////////////////////////////////////////////////////////////////////////////////////////////
+	//set some language-dependent strings
+	$pdf->setLanguageArray($l);
+	//////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	// // asigno la fuente del documento
-	// $pdf->SetFont('helvetica', '', 12);
+	// asigno la fuente del documento
+	$pdf->SetFont('helvetica', '', 12);
 	
-	// //Adiciono una pagina
-	// $pdf->AddPage();
+	//Adiciono una pagina
+	$pdf->AddPage();
 	
-	// //Cabeceras de las columnas
-	// $header = array('CODIGO', 'NOMBRES', 'APELLIDOS','VOTOS');
-	// $w = array(18, 38, 38,18); //Tamanyo de las columnas
+	//Cabeceras de las columnas
+	$header = array('CODIGO', 'NOMBRE', 'VOTOS');
+	$w = array(18,100,18); //Tamanyo de las columnas
 	
-	// //Inicio Iteracion
-	// $pdf->SetFillColor(255, 0, 0);
-	// $pdf->SetTextColor(255);
-	// $pdf->SetDrawColor(128, 0, 0);
-	// $pdf->SetLineWidth(0.3);
-	// $pdf->SetFont('', 'B');
+	//Inicio Iteracion
+	$pdf->SetFillColor(255, 0, 0);
+	$pdf->SetTextColor(255);
+	$pdf->SetDrawColor(128, 0, 0);
+	$pdf->SetLineWidth(0.3);
+	$pdf->SetFont('', 'B');
 	
-	// //Ajusto el texto a la celda con la opcion stretch o redusco el tamanyo
-	// $stretch = 0;
+	//Ajusto el texto a la celda con la opcion stretch o redusco el tamanyo
+	$stretch = 0;
 	
-	// // Header
-	// $num_headers = count($header);
-	// for($i = 0; $i < $num_headers; ++$i) {
-		// $pdf->Cell($w[$i], 6, $header[$i], 1, 0, 'C', 1,'',$stretch);
-	// }
-	// $pdf->Ln();
+	// Header
+	$num_headers = count($header);
+	for($i = 0; $i < $num_headers; ++$i) {
+		$pdf->Cell($w[$i], 6, $header[$i], 1, 0, 'C', 1,'',$stretch);
+	}
+	$pdf->Ln();
 	
-	// //Datos
-	// $pdf->SetFillColor(224, 235, 255);
-	// $pdf->SetTextColor(0);
-	// $pdf->SetFont('','',8);
+	//Datos
+	$pdf->SetFillColor(224, 235, 255);
+	$pdf->SetTextColor(0);
+	$pdf->SetFont('','',8);
 	
-	// $fill = 0;
-	// while($row = ibase_fetch_object($result)) {
-		// $pdf->Cell($w[0], 6, utf8_encode($row->CODIGO), 'LR', 0, 'L', $fill,'',$stretch);
-		// $pdf->Cell($w[1], 6, utf8_encode($row->NOMBRES), 'LR', 0, 'L', $fill,'',$stretch);
-		// $pdf->Cell($w[2], 6, utf8_encode($row->APELLIDOS), 'LR', 0, 'L', $fill,'',$stretch);
-		// $pdf->Cell($w[3], 6, $row->VOTOS, 'LR', 0, 'L', $fill,'',$stretch);
-		// $pdf->Ln();
-		// $fill=!$fill;
-	// }
-	// $pdf->Cell(array_sum($w), 0, '', 'T');
+	foreach($partidos as $partido) {
+		$pdf->Cell($w[0], 6, $partido->CODIGO, 'LR', 0, 'L', $fill,'',$stretch);
+		$pdf->Cell($w[1], 6, utf8_encode($partido->DESCRIPCION), 'LR', 0, 'L', $fill,'',$stretch);
+		$pdf->Cell($w[2], 6, $partido->VOTOS, 'LR', 0, 'L', $fill,'',$stretch);
+		$pdf->Ln();
+		$fill=!$fill;
+		foreach($candidatos as $candidato) {
+			if($candidato->CODPARTIDO == $partido->CODIGO) {
+				$pdf->Cell($w[0], 6, $partido->CODIGO.'-'.$candidato->CODCANDIDATO, 'LR', 0, 'L', $fill,'',$stretch);
+				$pdf->Cell($w[1], 6, utf8_encode($candidato->DESCRIPCION), 'LR', 0, 'L', $fill,'',$stretch);
+				$pdf->Cell($w[2], 6, $candidato->VOTOS, 'LR', 0, 'L', $fill,'',$stretch);
+				$pdf->Ln();
+			}
+		}
+		$fill=!$fill;
+	}
+	$pdf->Cell(array_sum($w), 0, '', 'T');
 	
-	// ibase_free_result($result);
-	// ibase_close($firebird);
+	ibase_free_result($result);
+	if($result1 != null){ibase_free_result($result1);}
+	ibase_close($firebird);
 	
-	// //Guardo el documento en el servidor
-	// $pdf->Output('consolidadoPartidoLista.pdf', 'D');
-	// unset($pdf);
+	//Guardo el documento en el servidor
+	$pdf->Output('consolidadoPartidoLista.pdf', 'D');
+	unset($pdf);
 ?>
