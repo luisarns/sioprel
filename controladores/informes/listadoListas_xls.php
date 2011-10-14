ade689a73c2146960b25f92f60b5fdfd23b46eda
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-10-14 10:29:56 -0500

    Punto 8, implementada la generación del reporte xls

--- controladores/informes/listadoListas_xls.php
+++ controladores/informes/listadoListas_xls.php
@@ -5,10 +5,7 @@
 	header('Cache-Control: max-age=0');
 	
 	require_once 'Configuracion.php';
-	
-	$datos = json_decode(str_replace("\\","",$_POST['datos']));
 	$datos = unserialize($_SESSION['listadoListas']);
-	
 	require_once 'listadoListas_inc.php';
 	
 	$objPHPExcel = new PHPExcel();
@@ -24,25 +21,24 @@
 	
 	
 	$objPHPExcel->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'PARTIDO')
-            ->setCellValue('B1', 'VOTOS')
-            ->setCellValue('C1', '(%)');
+            ->setCellValue('A1', 'LISTA')
+            ->setCellValue('B1', 'VOTOS');
 			
 	//Agregando los valores al informe
 	$cont = 2;
-	// while($row = ibase_fetch_object($result)) {
-		// $objPHPExcel->getActiveSheet()->setCellValue('A'.$cont,utf8_encode($row->PARTIDO));
-		// $objPHPExcel->getActiveSheet()->setCellValue('B'.$cont,$row->VOTOS);
-		// $objPHPExcel->getActiveSheet()->setCellValue('B'.$cont,$row->PORCENTAJE);
-		// $cont++;
-	// }
+	while($row = ibase_fetch_object($result)) {
+		$objPHPExcel->getActiveSheet()->setCellValue('A'.$cont,utf8_encode($row->DESCRIPCION));
+		$objPHPExcel->getActiveSheet()->setCellValue('B'.$cont,$row->VOTOS);
+		$cont++;
+	}
 
-	$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(100);
-	$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(14);
-	$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(10);
+	// $objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(100);
+	// $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(14);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
 	
-	// ibase_free_result($result);
-	// ibase_close($firebird);
+	ibase_free_result($result);
+	ibase_close($firebird);
 	
 	//Creando el escritor
 	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
