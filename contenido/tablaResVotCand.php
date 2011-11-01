a604f3242b7ba3f7c837661661d543f3b53c96d0
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-11-01 17:37:57 -0500

    Cambio la tabla de exportacion para el Punto 3.

--- contenido/tablaResVotCand.php
+++ contenido/tablaResVotCand.php
@@ -9,7 +9,7 @@
 	$codmunip = $_GET['municipio'];
 	
 	$txt = "";
-	if(isset($_GET['comuna']) && $_GET['comuna'] != "-"){
+	if(isset($_GET['comuna']) && $_GET['comuna'] != "-") {
 		$txt = "AND pc.idcomuna = ".$_GET['comuna'];
 		$txt .= " AND pd.idcomuna = ".$_GET['comuna'];
 	}
@@ -21,7 +21,8 @@
 	$cordivi = substr($codcordivi,0,getNumDigitos($nivcorpo));
 	
 	$query =<<<EOF
-	SELECT pc.codpartido || '-' || pc.codcandidato as codigo  ,pc.nombres, pc.apellidos ,sum(mv.numvotos) as votos
+	SELECT pc.codpartido || '-' || pc.codcandidato as codigo  ,pc.nombres, 
+	pc.apellidos ,sum(mv.numvotos) as votos
 	FROM pmesas pm, mvotos mv, pcandidatos pc, pdivipol pd
 	WHERE pd.coddivipol = pm.coddivipol AND pd.codnivel = 4
 	AND pd.coddivipol LIKE '$codcordivi' || '%' $txt
@@ -38,13 +39,12 @@ EOF;
 	
 ?>
 <table>
-<tr>
-	<td><h4>Descargar</h4></td>
-	<td><a href="<?php echo $urlReportes."pdf"?>" target="_BLANK"><img src="images/logo_pdf.png"  alt="pdf" height="35" width="35" /></a><td>
-	<td><a href="<?php echo $urlReportes."xls"?>" target="_BLANK"><img src="images/logo_xls.jpg"  alt="xls" height="35" width="35" /></a><td>
-	<td><a href="<?php echo $urlReportes."doc"?>" target="_BLANK"><img src="images/logo_doc.jpg"  alt="doc" height="35" width="35" /></a><td>
-	<td><a href="<?php echo $urlReportes."txt"?>" target="_BLANK"><img src="images/logo_text.jpg" alt="txt" height="35" width="35" /></a><td>
-</tr>
+	<tr>
+		<td><a href="<?php echo $urlReportes."pdf"?>" target="_BLANK"><img src="images/logo_pdf.png"  alt="pdf" height="20" width="20" /></a><td>
+		<td><a href="<?php echo $urlReportes."xls"?>" target="_BLANK"><img src="images/logo_xls.jpg"  alt="xls" height="20" width="20" /></a><td>
+		<td><a href="<?php echo $urlReportes."doc"?>" target="_BLANK"><img src="images/logo_doc.jpg"  alt="doc" height="20" width="20" /></a><td>
+		<td><a href="<?php echo $urlReportes."txt"?>" target="_BLANK"><img src="images/logo_text.jpg" alt="txt" height="20" width="20" /></a><td>
+	</tr>
 </table>
 
 <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
