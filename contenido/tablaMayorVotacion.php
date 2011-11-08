3303c087256093be44441990a89adec799cc6ae0
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-11-08 16:00:03 -0500

    Refactorizacion terminada para los ultimos 3 puntos.

--- contenido/tablaMayorVotacion.php
+++ contenido/tablaMayorVotacion.php
@@ -1,42 +1,42 @@
 <?php
-		require('conexion.php');
-		include_once('FunDivipol.php');
-		
-		$urlReportes = "http://" . $_SERVER['HTTP_HOST'] . "/reportes/repLisMayorVota.php" . $_SERVER['REQUEST_URI'];
-		$urlReportes .= "&formato=";
-		
-		
-		$codcorporacion = $_GET['corporacion'];
-		$nivcorpo  = getNivelCorporacion($codcorporacion);
-		
-		$depto = $_GET['departamento'];
-		$muncp = ($_GET['municipio'] != "-")? $_GET['municipio'] : "";
-		
-		$coddivcorto = $depto . $muncp;
-		$codcordiv   = substr($coddivcorto, 0, getNumDigitos($nivcorpo));
-		
-		$txt = "";
-		if (isset($_GET['comuna']) && $_GET['comuna'] != "-") {
-			$txt = "AND pc.idcomuna = " . $_GET['comuna'];
-			$txt .= " AND pd.idcomuna = " . $_GET['comuna'];
-		}
-		
-		$query =<<<EOF
-		SELECT pc.nombres as descripcion, SUM(mv.numvotos) as votos
-		FROM pcandidatos pc, pmesas pm, mvotos mv, pdivipol pd
-		WHERE pc.coddivipol LIKE '$codcordiv'   || '%' AND pc.codnivel = $nivcorpo AND pc.codcorporacion = $codcorporacion
-		AND pd.coddivipol   LIKE '$coddivcorto' || '%' AND pm.codtransmision = mv.codtransmision
-		AND pc.idcandidato = mv.idcandidato AND pc.codcandidato = 0
-		AND pm.codcorporacion = $codcorporacion
-		AND pd.coddivipol = pm.coddivipol AND pd.codnivel = 4 $txt
-		GROUP BY pc.nombres
-		ORDER BY votos DESC
+    require('conexion.php');
+    include_once('FunDivipol.php');
+
+    $urlReportes = "http://" . $_SERVER['HTTP_HOST'] . "/reportes/repLisMayorVota.php" . $_SERVER['REQUEST_URI'];
+    $urlReportes .= "&formato=";
+
+
+    $codcorporacion = $_GET['corporacion'];
+    $nivcorpo  = getNivelCorporacion($codcorporacion);
+
+    $depto = $_GET['departamento'];
+    $muncp = ($_GET['municipio'] != "-")? $_GET['municipio'] : "";
+
+    $coddivcorto = $depto . $muncp;
+    $codcordiv   = substr($coddivcorto, 0, getNumDigitos($nivcorpo));
+
+    $txt = "";
+    if (isset($_GET['comuna']) && $_GET['comuna'] != "-") {
+            $txt = "AND pc.idcomuna = " . $_GET['comuna'];
+            $txt .= " AND pd.idcomuna = " . $_GET['comuna'];
+    }
+
+    $query =<<<EOF
+    SELECT pc.nombres as descripcion, SUM(mv.numvotos) as votos
+    FROM pcandidatos pc, pmesas pm, mvotos mv, pdivipol pd
+    WHERE pc.coddivipol LIKE '$codcordiv'   || '%' AND pc.codnivel = $nivcorpo AND pc.codcorporacion = $codcorporacion
+    AND pd.coddivipol   LIKE '$coddivcorto' || '%' AND pm.codtransmision = mv.codtransmision
+    AND pc.idcandidato = mv.idcandidato AND pc.codcandidato = 0
+    AND pm.codcorporacion = $codcorporacion
+    AND pd.coddivipol = pm.coddivipol AND pd.codnivel = 4 $txt
+    GROUP BY pc.nombres
+    ORDER BY votos DESC
 EOF;
+
+    $firebird = ibase_connect($host, $username, $password) or die("No se pudo conectar a la base de datos: " . ibase_errmsg());
+    $result   = ibase_query($firebird, $query);
 		
-		$firebird = ibase_connect($host,$username,$password) or die("No se pudo conectar a la base de datos: " . ibase_errmsg());
-		$result   = ibase_query($firebird, $query);
-		
-	?>
+?>
 	
     <table>
         <tr>
@@ -93,20 +93,18 @@ EOF;
 	
 	<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
 		<tr>
-
-			<td background="../images/ds_comp_bari_gral.jpg">
-				<img src="../images/ds_comp_izq_bari_gral.jpg" width="25" height="25">
-			</td>
-			<td background="../images/ds_comp_bari_gral.jpg">&nbsp;</td>
-			<td align="right" background="../images/ds_comp_bari_gral.jpg">
-				<img src="../images/ds_comp_der_bari_gral.jpg" width="25" height="25">
-			</td>
-				
+                    <td background="../images/ds_comp_bari_gral.jpg">
+                            <img src="../images/ds_comp_izq_bari_gral.jpg" width="25" height="25">
+                    </td>
+                    <td background="../images/ds_comp_bari_gral.jpg">&nbsp;</td>
+                    <td align="right" background="../images/ds_comp_bari_gral.jpg">
+                            <img src="../images/ds_comp_der_bari_gral.jpg" width="25" height="25">
+                    </td>		
 		</tr>
 	</table>
 	<!-- Fin codigo estilo de tabla-->
 	
-	<?php 
-		ibase_free_result($result);
-		ibase_close($firebird);
-	?>
+<?php 
+        ibase_free_result($result);
+        ibase_close($firebird);
+?>
