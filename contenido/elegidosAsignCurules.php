bd2f6644393b0892342bba7ad86151c51ca945d7
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-11-08 17:23:08 -0500

    Refactorizacion terminada para los ultimos 3 puntos.

--- contenido/elegidosAsignCurules.php
+++ contenido/elegidosAsignCurules.php
@@ -9,89 +9,89 @@
 <form name="formPrincipal" method="GET" onSubmit="return validar(this);">
 	
 	<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-		<tr>
-			<td width="5%" background="../images/ds_comp_bars_gral.jpg">
-			<img src="../images/ds_comp_izq_bar_gral.jpg" width="25" height="25" />
-			</td>
-			<td width="83%" background="../images/ds_comp_bars_gral.jpg">
-                            <strong>Formulario Elegidos Asignaci&oacute;n Curules</strong>
-			</td>
-			<td width="12%" align="right" background="../images/ds_comp_bars_gral.jpg">
-				<img src="../images/ds_comp_der_bar_gral.jpg" width="25" height="25" />
-			</td>
-		</tr>
+            <tr>
+                <td width="5%" background="../images/ds_comp_bars_gral.jpg">
+                <img src="../images/ds_comp_izq_bar_gral.jpg" width="25" height="25" />
+                </td>
+                <td width="83%" background="../images/ds_comp_bars_gral.jpg">
+                    <strong>Formulario Elegidos Asignaci&oacute;n Curules</strong>
+                </td>
+                <td width="12%" align="right" background="../images/ds_comp_bars_gral.jpg">
+                        <img src="../images/ds_comp_der_bar_gral.jpg" width="25" height="25" />
+                </td>
+            </tr>
 	</table>
 	
 	<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-	<tr>
-		<td class="regOscuro" align="left">
-			<strong>&nbsp;</strong>
-		</td>
-	</tr>
+            <tr>
+                <td class="regOscuro" align="left">
+                        <strong>&nbsp;</strong>
+                </td>
+            </tr>
 	</table>
 	
 	<table width="100%" align="center" border="0" cellspacing="3" cellpadding="0" class="regSuave">
-		<tr>
-                    <td class="regSuaveleft">
-                        Corporaci&oacute;n&nbsp;
-                         <select name="corporacion" onChange="mostrarOcultarDepto(this.value)">
-                                <option value = "-" >-Ninguna-</option>
-                                <?php foreach($corporaciones as $corporacion){ ?>
-                                <option value="<?php echo $corporacion['id'] ?>" > <?php echo $corporacion['nombre'] ?> </option>
-                                <?php } ?>
-                        </select>
-                    </td>
-                </tr>
-                
-                <tr>
-                    <td id="divseldepto" style="display:none;">    
-                        Departamento : <select name="departamento" onChange="cargarMunicipios(this.value)">
-                        <option value = "-" >-Ninguna-</option>
-                        <?php foreach($departamentos as $departamento) { ?>
-                        <option value="<?php echo $departamento['id'] ?>" > <?php echo $departamento['nombre'] ?> </option>
-                        <?php } ?>
-                        </select>
-                    </td>
-		</tr>
-		
-		<tr>
-                    <td id="divselmunicipio" style="display:none;">
-                        Municipio : <select id="selmunicipio" name="municipio" onChange="alert('Cambio el municipio')">
-                        <option value = "-" > Seleccione un departamento </option> </select>
-                    </td>
-               </tr>
-                
-                <tr> 
-                    <td id="divselcomuna" style="display:none;">
-                        Comuna : <select id="selcomuna" name="comuna" onChange="alert('Cambio la comuna')">
-                        <option value = "-" > Seleccione un municipio </option> </select>
-                    </td>
-		</tr>
+            
+            <tr>
+                <td class="regSuaveleft">
+                    Corporaci&oacute;n&nbsp;
+                     <select name="corporacion" onChange="mostrarOcultarDepto(this.value)">
+                            <option value = "-" >-Ninguna-</option>
+                            <?php foreach($corporaciones as $corporacion){ ?>
+                            <option value="<?php echo $corporacion['id'] ?>" > <?php echo $corporacion['nombre'] ?> </option>
+                            <?php } ?>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td id="divseldepto" style="display:none;">    
+                    Departamento : <select name="departamento" onChange="cargarMunicipios(this.value)">
+                    <option value = "-" >-Ninguna-</option>
+                    <?php foreach($departamentos as $departamento) { ?>
+                    <option value="<?php echo $departamento['id'] ?>" > <?php echo $departamento['nombre'] ?> </option>
+                    <?php } ?>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td id="divselmunicipio" style="display:none;">
+                    Municipio : <select id="selmunicipio" name="municipio" onChange="alert('Cambio el municipio')">
+                    <option value = "-" > Seleccione un departamento </option> </select>
+                </td>
+           </tr>
+
+            <tr> 
+                <td id="divselcomuna" style="display:none;">
+                    Comuna : <select id="selcomuna" name="comuna" onChange="alert('Cambio la comuna')">
+                    <option value = "-" > Seleccione un municipio </option> </select>
+                </td>
+            </tr>
                 
 	</table>
 	
 	
 	<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-		<tr>
-			<td align="center" class='regOscuroCenter'>   
-				<input type="submit" class="hospital" name="consultar" value="Consultar"/>
-				<!--<input type="reset" class="hospital" name="limpiar" value="Limpiar" />-->
-			</td>
-		</tr>
+            <tr>
+                <td align="center" class='regOscuroCenter'>   
+                        <input type="submit" class="hospital" name="consultar" value="Consultar"/>
+                </td>
+            </tr>
 	</table>
 
 	<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-		<tr>
+            <tr>
 
-			<td background="../images/ds_comp_bari_gral.jpg">
-				<img src="../images/ds_comp_izq_bari_gral.jpg" width="25" height="25">
-			</td>
-			<td background="../images/ds_comp_bari_gral.jpg">&nbsp;</td>
-			<td align="right" background="../images/ds_comp_bari_gral.jpg">
-				<img src="../images/ds_comp_der_bari_gral.jpg" width="25" height="25">
-			</td>
-				
-		</tr>
+                <td background="../images/ds_comp_bari_gral.jpg">
+                        <img src="../images/ds_comp_izq_bari_gral.jpg" width="25" height="25">
+                </td>
+                <td background="../images/ds_comp_bari_gral.jpg">&nbsp;</td>
+                <td align="right" background="../images/ds_comp_bari_gral.jpg">
+                        <img src="../images/ds_comp_der_bari_gral.jpg" width="25" height="25">
+                </td>
+
+            </tr>
 	</table>
 	
 	<input type="hidden" name="opcion"  value="8"/>
