8d1628d25a8169b9e36715d4dc8a4ac2be071c05
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-11-10 09:46:00 -0500

    Punto 1, refactorizado version alfa.

--- reportes/consolidadoPartidoLista_otr.php
+++ reportes/consolidadoPartidoLista_otr.php
@@ -3,18 +3,18 @@
 	require_once('consolidadoPartidoLista_inc.php');
 	
 	//Configuracion para la generacion del pdf
-	$partidos = array();
-	$candidatos = array();
-	
-	while($row = ibase_fetch_object($result)) {
-		array_push($partidos,$row);
-	}
-	
-	if($result1 != null) {
-		while($row = ibase_fetch_object($result1)) {
-			array_push($candidatos,$row);
-		}
-	}
+//	$partidos = array();
+//	$candidatos = array();
+//	
+//	while($row = ibase_fetch_object($result)) {
+//		array_push($partidos,$row);
+//	}
+//	
+//	if($result1 != null) {
+//		while($row = ibase_fetch_object($result1)) {
+//			array_push($candidatos,$row);
+//		}
+//	}
 	
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
@@ -27,20 +27,30 @@
 							 ->setDescription("Consolidado Partido Lista")
 							 ->setKeywords("office 2007 openxml")
 							 ->setCategory("");
+        
+        
 	
 	$objPHPExcel->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'CODIGO')
-            ->setCellValue('B1', 'NOMBRE')
-            ->setCellValue('C1', 'VOTOS');
-			
+            ->setCellValue('A3', utf8_encode('CÓDIGO'))
+            ->setCellValue('B3', 'NOMBRE')
+            ->setCellValue('C3', 'VOTOS')
+            ->setCellValue('D3', utf8_encode('PARTICIPACIÓN'));
+        
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', utf8_encode('Participación'))
+            ->setCellValue('B1', $participacion . '%')
+            ->setCellValue('A2', utf8_encode('Abstención'))
+            ->setCellValue('B2', $asbtencion . '%');
+
+
 	//Configurado del contenido
-	$cont = 2;
+	$cont = 4;
 	foreach($partidos as $partido) {
 		
 		$objPHPExcel->getActiveSheet()->setCellValue('A'.$cont, str_pad($partido->CODIGO, 3, '0', STR_PAD_LEFT));
 		$objPHPExcel->getActiveSheet()->setCellValue('B'.$cont, utf8_encode($partido->DESCRIPCION));
 		$objPHPExcel->getActiveSheet()->setCellValue('C'.$cont, number_format($partido->VOTOS));
-		//Hacer un cambio de color para las celdas de los partidos
+                $objPHPExcel->getActiveSheet()->setCellValue('D'.$cont, round($partido->VOTOS*100/$potencial,2) . '%');
 		$cont++;
 		
 		foreach($candidatos as $candidato) {
@@ -52,16 +62,24 @@
 			}
 		}
 	}
+        foreach ($votacionEspecial as $votoEsp) {
+            $objPHPExcel->getActiveSheet()->setCellValue('A'.$cont,'');
+            $objPHPExcel->getActiveSheet()->setCellValue('B'.$cont, utf8_encode($votoEsp->DESCRIPCION));
+            $objPHPExcel->getActiveSheet()->setCellValue('C'.$cont, number_format($votoEsp->VOTOS));
+            $objPHPExcel->getActiveSheet()->setCellValue('D'.$cont, round($votoEsp->VOTOS*100/$potencial,2) . '%');
+            $cont++;
+        }
 	
 	$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
 	$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
 	$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
 	//Fin del contenido
 	
 	//Cerrando la conexion
-	ibase_free_result($result);
-	if($result1 != null){ibase_free_result($result1);}
-	ibase_close($firebird);
+//	ibase_free_result($result);
+//	if($result1 != null){ibase_free_result($result1);}
+//	ibase_close($firebird);
 	
 	//Creando el escritor en funcion del formato al que se quiera exportar el documento
 	switch($_GET['formato']) {
