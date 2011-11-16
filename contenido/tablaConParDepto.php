a377dabf7618713eb83c7782b674fce89f500d7e
Luis A. Sanchez <lnunez@.DCSIO.SIO>
2011-11-16 11:13:39 -0500

    Punto 2, definicion de las consultas terminada.

--- contenido/tablaConParDepto.php
+++ contenido/tablaConParDepto.php
@@ -8,7 +8,6 @@
     $corporacion  = $_GET['corporacion'];
     $departamento = $_GET['departamento'];
     
-    //El codigo corto de la divipol y su nivel
     $coddivipol = $departamento;
     $codnivel = 1; 
     
@@ -18,10 +17,65 @@
     }
     
     $txt = ""; //Filtro para la comuna
+    $txt1 = "";
     if ($_GET['comuna'] !='-') {
-        
+        $txt = " AND pd.idcomuna = " . $_GET['comuna'];
+        $txt .= " AND pc.idcomuna = " . $_GET['comuna'];
+        $txt1 = " AND pc.idcomuna = " . $_GET['comuna'];
     }
     
-    echo "divipol : " . str_pad($coddivipol,9,'0') . " codnivel : $codnivel";
-    sleep(3);
+    $nivcorpo = getNivelCorporacion($corporacion);
+    $coddivcorpo = str_pad(substr($coddivipol, 0, getNumDigitos($nivcorpo)),9,'0');
+    
+    
+    $queryVotacionPartido =<<<VTP
+        SELECT pp.codpartido, pp.descripcion, sum(mv.numvotos) as votos
+        FROM ppartidos pp, pcandidatos pc, pdivipol pd, pmesas pm, mvotos mv
+        WHERE pp.codpartido = pc.codpartido
+        AND pc.coddivipol = '$coddivcorpo'
+        AND pd.coddivipol LIKE '$coddivipol' || '%'
+        AND pd.codnivel = 4
+        AND pd.coddivipol = pm.coddivipol
+        AND pc.codnivel = $nivcorpo
+        AND pc.codcorporacion = $corporacion
+        AND pm.codcorporacion = $corporacion
+        AND pm.codtransmision = mv.codtransmision
+        AND pc.idcandidato = mv.idcandidato
+        $txt
+        GROUP BY pp.codpartido, pp.descripcion
+VTP;
+    
+    $queryPartidoAvalados = <<<PAV
+        SELECT pp.codpartido, pp.descripcion, count(pc.idcandidato) as avalados
+        FROM ppartidos pp, pcandidatos pc
+        WHERE pp.codpartido = pc.codpartido
+        AND pc.coddivipol = '$coddivcorpo'
+        AND pc.codcorporacion = $corporacion
+        AND pc.codcandidato <> 0
+        AND pc.codnivel = $nivcorpo
+        $txt1
+        GROUP BY pp.codpartido, pp.descripcion
+PAV;
+    
+    $queryPartidoElegidos = <<<PEL
+        SELECT pp.codpartido, pp.descripcion, count(pc.idcandidato) as elegidos
+        FROM ppartidos pp, pcandidatos pc
+        WHERE pp.codpartido = pc.codpartido
+        AND pc.coddivipol = '$coddivcorpo'
+        AND pc.codcorporacion = $corporacion
+        AND pc.codcandidato <> 0
+        AND pc.codnivel = $nivcorpo
+        AND pc.elegido <> '0'
+        $txt1
+        GROUP BY pp.codpartido, pp.descripcion
+PEL;
+    
+    echo $queryVotacionPartido;
+    echo "<br/><br/>";
+    echo $queryPartidoAvalados;
+    echo "<br/><br/>";
+    echo $queryPartidoElegidos;
+    
+    
+    
 ?>
